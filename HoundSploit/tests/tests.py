31da82902792d79a60aef1b433406889c456515b
nicolas-carolo <nicolas.carolo@outlook.com>
2018-11-22 17:10:02 +0100

    Now unit test pass correctly

--- HoundSploit/tests/tests.py
+++ HoundSploit/tests/tests.py
@@ -1,10 +1,2 @@
-from django.urls import resolve
-from django.test import TestCase
-from HoundSploit.views import home_page
 
 
-class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
--- searcher/tests/tests.py
+++ searcher/tests/tests.py
@@ -1,11 +1,11 @@
 from django.urls import resolve
 from django.test import TestCase
-from HoundSploit.views import home_page
+from searcher.views import home_page
 
 
 class TestPageTest(TestCase):
 
     def test_root_url_resolves_to_search_page_view(self):
-        found = resolve('/search')
+        found = resolve('/')
         self.assertEqual(found.func, home_page)
 
