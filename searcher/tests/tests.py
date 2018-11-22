51de1af973c1ae22588ba97ded1695592e031d84
nicolas-carolo <nicolas.carolo@outlook.com>
2018-11-22 17:40:59 +0100

    I have edited unit test

--- searcher/tests/tests.py
+++ searcher/tests/tests.py
@@ -1,11 +1,9 @@
-from django.urls import resolve
 from django.test import TestCase
-from searcher.views import home_page
 
 
 class TestPageTest(TestCase):
 
     def test_root_url_resolves_to_search_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
