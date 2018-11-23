56f8c06aaf1ff29a9e684db6d5047a2b9d9c2e89
nicolas-carolo <nicolas.carolo@outlook.com>
2018-11-24 00:29:46 +0100

    I have developed a basic search function

--- searcher/views.py
+++ searcher/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
+from searcher.models import Exploit
 
 
 def home_page(request):
     if request.POST:
-        print(request.POST['term'])
+        search_string = 'select * from exploits where ' + request.POST['search_item']
+        print(search_string)
+        for exploit in Exploit.objects.raw(search_string):
+            print(exploit.id, exploit.file, exploit.description)
         return redirect("/")
     else:
         return render(request, 'home.html')
