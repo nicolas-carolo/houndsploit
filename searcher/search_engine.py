9d09c798f2126490a81465489c694271a62de5ed
nicolas-carolo <nicolas.carolo@outlook.com>
2018-11-24 15:17:43 +0100

    Layout improvements

--- searcher/search_engine.py
+++ searcher/search_engine.py
@@ -5,7 +5,7 @@ def search_exploits_in_db(search_text):
     if is_number(search_text):
         search_string = 'select * from exploits where ' + 'id = ' + search_text + ' or file like \'%' + search_text + '%\' or description like \'%' + search_text + '%\' or port = ' + search_text
     else:
-        search_string = 'select * from exploits where ' + 'file like \'%' + search_text + '%\' or description like \'%' + search_text + '%\' or author like \'%' + search_text + '\' or exploit_type like \'%' + search_text + '%\' or platform like \'%' + search_text + '%\''
+        search_string = 'select * from exploits where ' + 'file like \'%' + search_text + '%\' or description like \'%' + search_text + '%\' or author like \'%' + search_text + '%\' or exploit_type like \'%' + search_text + '%\' or platform like \'%' + search_text + '%\''
     print(search_string)
     return Exploit.objects.raw(search_string)
 
