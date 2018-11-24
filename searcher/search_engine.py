113e595336c425308371dad45bd134d1b703862b
nicolas-carolo <nicolas.carolo@outlook.com>
2018-11-24 12:31:35 +0100

    Now HoundSploit can perform a basic search (type excluded)

--- searcher/search_engine.py
+++ searcher/search_engine.py
@@ -2,8 +2,17 @@ from searcher.models import Exploit
 
 
 def search_exploits_in_db(search_text):
-    search_string = 'select * from exploits where ' + search_text
+    if is_number(search_text):
+        search_string = 'select * from exploits where ' + 'id = ' + search_text + ' or file like \'%' + search_text + '%\' or description like \'%' + search_text + '%\' or port = ' + search_text
+    else:
+        search_string = 'select * from exploits where ' + 'file like \'%' + search_text + '%\' or description like \'%' + search_text + '%\' or author like \'%' + search_text + '\' or platform like \'%' + search_text + '%\''
     print(search_string)
-    for exploit in Exploit.objects.raw(search_string):
-        print(exploit.id, exploit.file, exploit.description)
     return Exploit.objects.raw(search_string)
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
