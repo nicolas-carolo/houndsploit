a5a143c7d9ca8ae29aaad24d21385ceabe2dd0a5
nicolas-carolo <nicolas.carolo@outlook.com>
2018-11-26 17:25:53 +0100

    Now HoundSploit shows the number of results found

--- searcher/views.py
+++ searcher/views.py
@@ -6,6 +6,7 @@ def home_page(request):
     if request.POST:
         search_text = request.POST['search_item']
         return render(request, "home.html", {'searched_item': str(search_text),
-                                             'result': search_exploits_in_db(search_text)})
+                                             'results': search_exploits_in_db(search_text),
+                                             'n_results': len(search_exploits_in_db(search_text))})
     else:
         return render(request, 'home.html')
