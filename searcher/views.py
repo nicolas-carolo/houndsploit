55db6719e6be5f99f558896231c7088e4c0433d5
nicolas-carolo <nicolas.carolo@outlook.com>
2018-11-26 17:02:44 +0100

    I have added string 'Results found for...'

--- searcher/views.py
+++ searcher/views.py
@@ -5,6 +5,7 @@ from searcher.search_engine import search_exploits_in_db
 def home_page(request):
     if request.POST:
         search_text = request.POST['search_item']
-        return render(request, "home.html", {'result': search_exploits_in_db(search_text)})
+        return render(request, "home.html", {'searched_item': str(search_text),
+                                             'result': search_exploits_in_db(search_text)})
     else:
         return render(request, 'home.html')
