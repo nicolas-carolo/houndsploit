3d6bd27740b050c1fe274ac316306e910cbd73c7
nicolas-carolo <nicolas.carolo@outlook.com>
2018-11-26 17:59:05 +0100

    Re-organization of urls

--- searcher/views.py
+++ searcher/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render, redirect
 from searcher.search_engine import search_exploits_in_db
 
 
+def get_results_table(request):
+    if request.POST:
+        search_text = request.POST['search_item']
+        return render(request, "results_table.html", {'searched_item': str(search_text),
+                                                      'results': search_exploits_in_db(search_text),
+                                                      'n_results': len(search_exploits_in_db(search_text))})
+    else:
+        return render(request, 'results_table.html')
+
+
 def home_page(request):
     if request.POST:
         search_text = request.POST['search_item']
-        return render(request, "home.html", {'searched_item': str(search_text),
-                                             'results': search_exploits_in_db(search_text),
-                                             'n_results': len(search_exploits_in_db(search_text))})
+        return render(request, "results_table.html", {'searched_item': str(search_text),
+                                                      'results': search_exploits_in_db(search_text),
+                                                      'n_results': len(search_exploits_in_db(search_text))})
     else:
         return render(request, 'home.html')
+
