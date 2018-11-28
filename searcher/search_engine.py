217997a97c27c81913bd7c3dbc4589b0a5dd701e
nicolas <nicolas.carolo@outlook.com>
2018-11-28 21:45:27 +0100

    Improvements in searching

--- searcher/search_engine.py
+++ searcher/search_engine.py
@@ -6,9 +6,9 @@ def search_exploits_in_db(search_text):
         search_string = 'select * from exploits where ' + 'id = ' + search_text + ' or file like \'%' + search_text + '%\' or description like \'%' + search_text + '%\' or port = ' + search_text
     else:
         words_list = str(search_text).split()
-        search_concat_text = ''
+        search_concat_text = '%'
         for word in words_list:
-            search_concat_text = search_concat_text + '%' + word.upper() + '%'
+            search_concat_text = search_concat_text + word.upper() + '%'
         search_string = 'select * from exploits where ' + 'file like \'' + search_concat_text + '\' or description like \'' + search_concat_text + '\' or author like \'' + search_concat_text + '\' or exploit_type like \'' + search_concat_text + '\' or platform like \'' + search_concat_text + '\''
     print(search_string)
     return Exploit.objects.raw(search_string)
