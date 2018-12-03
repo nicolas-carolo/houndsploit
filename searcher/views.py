c725852e2baa3bae8b9390484188896ac2186b3d
nicolas-carolo <nicolas.carolo@outlook.com>
2018-12-03 12:32:34 +0100

    I have edited methods for preparing the app to search also shellcodes

--- searcher/views.py
+++ searcher/views.py
@@ -7,8 +7,8 @@ def get_results_table(request):
     if request.POST and is_valid_input(request.POST['search_item']):
         search_text = request.POST['search_item']
         return render(request, "results_table.html", {'searched_item': str(search_text),
-                                                      'results': search_vulnerabilities_in_db(),
-                                                      'n_results': len(search_vulnerabilities_in_db(search_text))})
+                                                      'exploits_results': search_vulnerabilities_in_db(),
+                                                      'n_exploits_results': len(search_vulnerabilities_in_db(search_text))})
     else:
         return render(request, 'results_table.html')
 
@@ -17,8 +17,8 @@ def home_page(request):
     if request.POST and is_valid_input(request.POST['search_item']):
         search_text = request.POST['search_item']
         return render(request, "results_table.html", {'searched_item': str(search_text),
-                                                      'results': search_vulnerabilities_in_db(search_text),
-                                                      'n_results': len(search_vulnerabilities_in_db(search_text))})
+                                                      'exploits_results': search_vulnerabilities_in_db(search_text),
+                                                      'n_exploits_results': len(search_vulnerabilities_in_db(search_text))})
     else:
         return render(request, 'home.html')
 
