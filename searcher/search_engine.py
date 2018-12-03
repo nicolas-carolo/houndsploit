cd3cf276780740acfc7e9567d4b078059633d8b9
nicolas-carolo <nicolas.carolo@outlook.com>
2018-12-03 17:28:29 +0100

    Bug fix and layout improvements

--- searcher/search_engine.py
+++ searcher/search_engine.py
@@ -3,8 +3,11 @@ from searcher.models import Exploit, Shellcode
 
 def search_vulnerabilities_in_db(search_text, vulnerability_type):
     words = str(search_text).split()
-    if words[0] == '--exact' and '--in' in words:
+    if (words[0] == '--exact' and '--in' in words) and vulnerability_type == 'exploits':
         return search_exploits_exact(words[1:])
+
+    if (words[0] == '--exact' and '--in' in words) and vulnerability_type == 'shellcodes':
+        return search_shellcodes_exact(words[1:])
 
     if str(search_text).isnumeric():
         return search_vulnerabilities_numerical(search_text, vulnerability_type)
@@ -81,6 +84,7 @@ def search_vulnerabilities_for_author_platform_type(search_text, vulnerability_t
 
 
 def search_exploits_exact(words):
+    print('exploits')
     accepted_fileds = ['file', 'description', 'author', 'type', 'platform', 'port']
     search_string = words[0]
     words_index = 1
@@ -94,12 +98,29 @@ def search_exploits_exact(words):
                 search_string = 'blablabla'
             if words[words_index + 1] == 'type':
                 words[words_index + 1] = 'vulnerability_type'
-            print('select * from exploits where ' + words[words_index + 1] + ' = \'%' + search_string.upper() + '%\'')
             if words[words_index + 1] == 'port' and search_string.isnumeric():
                 return Exploit.objects.raw('select * from exploits where port = ' + search_string.upper())
 
             else:
                 return Exploit.objects.raw('select * from exploits where ' + words[words_index + 1] + ' like \'%' + search_string.upper() + '%\'')
+
+
+def search_shellcodes_exact(words):
+    print('shellcodes')
+    accepted_fileds = ['file', 'description', 'author', 'type', 'platform']
+    search_string = words[0]
+    words_index = 1
+    for word in words[1:]:
+        if word != '--in':
+            search_string = search_string + ' ' + word
+            words_index = words_index + 1
+        else:
+            if words[words_index + 1] not in accepted_fileds:
+                words[words_index + 1] = 'description'
+                search_string = 'blablabla'
+            if words[words_index + 1] == 'type':
+                words[words_index + 1] = 'vulnerability_type'
+            return Exploit.objects.raw('select * from shellcodes where ' + words[words_index + 1] + ' like \'%' + search_string.upper() + '%\'')
 
 
 def is_valid_input(string):
