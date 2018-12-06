280070b7ce1da0fb8a1c598303f21fcd0d38095a
nicolas <nicolas.carolo@outlook.com>
2018-12-06 22:19:28 +0100

    Now you can inspect code clicking on the relative button

--- searcher/views.py
+++ searcher/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from searcher.search_engine import search_vulnerabilities_in_db
 from searcher.search_engine import is_valid_input
 from searcher.models import Exploit
+import os
 
 
 def get_results_table(request):
@@ -19,8 +20,12 @@ def get_results_table(request):
 
 def view_exploit_code(request, exploit_id):
     exploit = Exploit.objects.get(id=exploit_id)
-    print('exploit', exploit.file)
-    return render(request, 'code_viewer.html', {'exploit_file': exploit.file})
+    pwd = os.path.dirname(__file__)
+    file = open(pwd + '/static/vulnerability/' + exploit.file, 'r')
+    exploit_code = ''
+    for line in file:
+        exploit_code = exploit_code + line
+    return render(request, 'code_viewer.html', {'exploit_code': exploit_code})
 
 
 
