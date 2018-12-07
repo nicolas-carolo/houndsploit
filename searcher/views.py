60a441346c30a105df5470856438437258300dca
nicolas-carolo <nicolas.carolo@outlook.com>
2018-12-07 12:30:05 +0100

    Vulnerability info are shown in code_viewer.html

--- searcher/views.py
+++ searcher/views.py
@@ -20,22 +20,51 @@ def get_results_table(request):
 
 def view_exploit_code(request, exploit_id):
     exploit = Exploit.objects.get(id=exploit_id)
+    exploit_description = exploit.description
+    exploit_file = exploit.file
+    exploit_author = exploit.author
+    exploit_date = exploit.date
+    exploit_type = exploit.vulnerability_type
+    exploit_platform = exploit.platform
+    exploit_port = exploit.port
+
     pwd = os.path.dirname(__file__)
     file = open(pwd + '/static/vulnerability/' + exploit.file, 'r')
     vulnerability_code = ''
     for line in file:
         vulnerability_code = vulnerability_code + line
-    return render(request, 'code_viewer.html', {'vulnerability_code': vulnerability_code})
+    return render(request, 'code_viewer.html', {'vulnerability_code': vulnerability_code,
+                                                'vulnerability_description': exploit_description,
+                                                'vulnerability_file': exploit_file,
+                                                'vulnerability_author': exploit_author,
+                                                'vulnerability_date': exploit_date,
+                                                'vulnerability_type': exploit_type,
+                                                'vulnerability_platform': exploit_platform,
+                                                'vulnerability_port': exploit_port
+                                                })
 
 
 def view_shellcode_code(request, shellcode_id):
     shellcode = Shellcode.objects.get(id=shellcode_id)
+    shellcode_description = shellcode.description
+    shellcode_file = shellcode.file
+    shellcode_author = shellcode.author
+    shellcode_date = shellcode.date
+    shellcode_type = shellcode.vulnerability_type
+    shellcode_platform = shellcode.platform
     pwd = os.path.dirname(__file__)
     file = open(pwd + '/static/vulnerability/' + shellcode.file, 'r')
     vulnerability_code = ''
     for line in file:
         vulnerability_code = vulnerability_code + line
-    return render(request, 'code_viewer.html', {'vulnerability_code': vulnerability_code})
+    return render(request, 'code_viewer.html', {'vulnerability_code': vulnerability_code,
+                                                'vulnerability_description': shellcode_description,
+                                                'vulnerability_file': shellcode_file,
+                                                'vulnerability_author': shellcode_author,
+                                                'vulnerability_date': shellcode_date,
+                                                'vulnerability_type': shellcode_type,
+                                                'vulnerability_platform': shellcode_platform,
+                                                })
 
 
 
