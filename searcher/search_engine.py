26bf29186ad2695aa4f8356e03a4ed65d395b6f5
nicolas-carolo <nicolas.carolo@outlook.com>
2019-01-21 18:10:41 +0100

    Refactor and bug fix in is_in_version_range_with_x

--- searcher/search_engine.py
+++ searcher/search_engine.py
@@ -166,16 +166,14 @@ def is_in_version_range(num_version, software_name, description):
 def is_in_version_range_with_x(num_version, software_name, description):
     software_name = software_name.upper()
     description = description.upper()
-    regex = re.search(software_name + r' (\w+\.\w+\.\w+\.\w+|\w+\.\w+\.\w+|\w+\.\w+|\w+)(\.X)? < (\w+\.\w+\.\w+\.\w+|\w+\.\w+\.\w+|\w+\.\w+|\w+)(\.X)?', description)
+    regex = re.search(software_name + r' \w+((\.\w+)+)?(\.x)? < \w+((\.\w+)+)?(\.x)?', description)
     try:
         software = regex.group(0)
         regex = re.search(
-            r'(?P<from_version>(\w+\.\w+\.\w+\.\w+|\w+\.\w+\.\w+|\w+\.\w+|\w+))(\.X)? < (?P<to_version>(\w+\.\w+\.\w+\.\w+|\w+\.\w+\.\w+|\w+\.\w+|\w+))(\.X)?',
+            r'(?P<from_version>\d+((\.\d+)+)?)(\.X)? < (?P<to_version>\d+((\.\d+)+)?(\.X)?)',
             software)
         from_version = regex.group('from_version')
         to_version = regex.group('to_version')
-        regex = re.search(r'(?P<from_version>(\d+\.\d+\.\d+\.\d+|\d+\.\d+\.\d+|\d+\.\d+|\d+))($|\.X)', from_version)
-        from_version = regex.group('from_version')
         regex = re.search(r'(?P<base>\w+)\.(?P<least_digit>\d+)($|\.X)', to_version)
         if to_version.__contains__('X'):
             least_digit = int(regex.group('least_digit')) + 1
@@ -183,7 +181,7 @@ def is_in_version_range_with_x(num_version, software_name, description):
             least_digit = int(regex.group('least_digit'))
         to_version = regex.group('base') + '.' + str(least_digit)
         if parse_version(num_version) >= parse_version(from_version) and parse_version(
-                num_version) <= parse_version(to_version):
+                num_version) < parse_version(to_version):
             return True
         else:
             return False
