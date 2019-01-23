2712393ffd201cb90516431d8fcfa43c61c0a2c8
nicolas-carolo <nicolas.carolo@outlook.com>
2019-01-23 15:45:14 +0100

    pydoc of is_date_range_valid method.

--- searcher/engine/date_validator.py
+++ searcher/engine/date_validator.py
@@ -2,6 +2,13 @@ import datetime
 
 
 def is_date_range_valid(start_date, end_date):
+    """
+    Check if the lower range value is smaller than the highest and if the two date are both in the past or in the
+    present or if the two date fields are both void.
+    :param start_date: the lower range value.
+    :param end_date: the highest range value.
+    :return: True only if the range is valid or the two fields are both void.
+    """
     if start_date is not None and end_date is not None:
         if start_date <= end_date <= datetime.datetime.now().date():
             return True
