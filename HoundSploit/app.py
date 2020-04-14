d4c5ebd427a05f9990bea2d48ead0fb4cbbf98df
Nicolas Carolo <nicolas.carolo@outlook.com>
2020-04-14 23:31:27 +0200

    Download vulnerabilities and check on date range in advanced search

--- HoundSploit/app.py
+++ HoundSploit/app.py
@@ -10,8 +10,9 @@ from HoundSploit.searcher.engine.keywords_highlighter import highlight_keywords_
 from HoundSploit.searcher.engine.suggestions import substitute_with_suggestions, propose_suggestions, get_suggestions_list,\
     new_suggestion, remove_suggestion
 from HoundSploit.searcher.engine.updates import get_latest_db_update_date, install_updates
-from HoundSploit.searcher.engine.utils import check_file_existence
+from HoundSploit.searcher.engine.utils import check_file_existence, get_vulnerability_extension
 from HoundSploit.searcher.engine.csv2sqlite import create_db
+from shutil import copyfile
 
 
 init_path = os.path.expanduser("~") + "/HoundSploit"
@@ -74,13 +75,14 @@ def get_results_table_advanced():
                                    vulnerability_platforms_list=vulnerability_platforms_list)
         key_words_list = (str(searched_text).upper()).split()
 
+        date_alert = None
         try:
             date_from = datetime.datetime.strptime(date_from_filter, '%Y-%m-%d')
             date_to = datetime.datetime.strptime(date_to_filter, '%Y-%m-%d')
             if date_from > date_to:
                 date_from_filter = "mm/dd/yyyy"
                 date_to_filter = "mm/dd/yyyy"
-                # TODO implement javascript error
+                date_alert = "ERROR: date range not valid!"
         except ValueError:
             date_from_filter = "mm/dd/yyyy"
             date_to_filter = "mm/dd/yyyy"
@@ -106,7 +108,7 @@ def get_results_table_advanced():
                                vulnerability_platforms_list=vulnerability_platforms_list, author_filter=author_filter,
                                type_filter=type_filter, platform_filter=platform_filter, port_filter=port_filter,
                                date_from_filter=date_from_filter, date_to_filter=date_to_filter,
-                               suggested_search_text=suggested_search_text)
+                               suggested_search_text=suggested_search_text, date_alert=date_alert)
     else:
         return render_template('advanced_searcher.html', vulnerability_types_list=vulnerability_types_list,
                                vulnerability_platforms_list=vulnerability_platforms_list)
@@ -125,12 +127,37 @@ def view_exploit_details():
         with open(file_path, 'r') as f:
             content = f.readlines()
             vulnerability_code = ''.join(content)
-        file_name = exploit.description + get_vulnerability_extension(exploit.file)
         return render_template('code_viewer.html', vulnerability_code=vulnerability_code,
                                vulnerability_description=exploit.description, vulnerability_file=exploit.file,
                                vulnerability_author=exploit.author, vulnerability_date=exploit.date,
                                vulnerability_type=exploit.type, vulnerability_platform=exploit.platform,
-                               vulnerability_port=exploit.port, file_path=file_path, file_name=file_name)
+                               vulnerability_port=exploit.port, file_path=file_path, exploit_id=exploit_id)
+    except FileNotFoundError:
+        error_msg = 'Sorry! This file does not exist :('
+        return render_template('error_page.html', error=error_msg)
+
+
+@app.route('/download-exploit')
+def download_exploit_details():
+    """
+    Download the selected exploit.
+    :return: a template showing the details about the selected exploit and the source code.
+    """
+    exploit_id = request.args.get('exploit-id', None)
+    exploit = get_exploit_by_id(exploit_id)
+    file_path = init_path + "/exploitdb/" + exploit.file
+    try:
+        with open(file_path, 'r') as f:
+            content = f.readlines()
+            vulnerability_code = ''.join(content)
+        copyfile(file_path, os.path.expanduser("~") + "/exploit_" + exploit_id + get_vulnerability_extension(exploit.file))
+        download_alert = "exploit_" + exploit_id + get_vulnerability_extension(exploit.file) + " has been downloaded in you home directory"
+        return render_template('code_viewer.html', vulnerability_code=vulnerability_code,
+                               vulnerability_description=exploit.description, vulnerability_file=exploit.file,
+                               vulnerability_author=exploit.author, vulnerability_date=exploit.date,
+                               vulnerability_type=exploit.type, vulnerability_platform=exploit.platform,
+                               vulnerability_port=exploit.port, file_path=file_path, download_alert=download_alert,
+                               exploit_id=exploit_id)
     except FileNotFoundError:
         error_msg = 'Sorry! This file does not exist :('
         return render_template('error_page.html', error=error_msg)
@@ -149,12 +176,36 @@ def view_shellcode_details():
         with open(file_path, 'r') as f:
             content = f.readlines()
             vulnerability_code = ''.join(content)
-        file_name = shellcode.description + get_vulnerability_extension(shellcode.file)
         return render_template('code_viewer.html', vulnerability_code=vulnerability_code,
                                vulnerability_description=shellcode.description, vulnerability_file=shellcode.file,
                                vulnerability_author=shellcode.author, vulnerability_date=shellcode.date,
                                vulnerability_type=shellcode.type, vulnerability_platform=shellcode.platform,
-                               file_path=file_path, file_name=file_name)
+                               file_path=file_path, shellcode_id=shellcode_id)
+    except FileNotFoundError:
+        error_msg = 'Sorry! This file does not exist :('
+        return render_template('error_page.html', error=error_msg)
+
+
+@app.route('/download-shellcode')
+def download_shellcode():
+    """
+    Download the selected shellcode.
+    :return: a template showing the details about the selected shellcode and the source code.
+    """
+    shellcode_id = request.args.get('shellcode-id', None)
+    shellcode = get_shellcode_by_id(shellcode_id)
+    file_path = init_path + "/exploitdb/" + shellcode.file
+    try:
+        with open(file_path, 'r') as f:
+            content = f.readlines()
+            vulnerability_code = ''.join(content)
+        copyfile(file_path, os.path.expanduser("~") + "/shellcode_" + shellcode_id + get_vulnerability_extension(shellcode.file))
+        download_alert = "shellcode_" + shellcode_id + get_vulnerability_extension(shellcode.file) + " has been downloaded in you home directory"
+        return render_template('code_viewer.html', vulnerability_code=vulnerability_code,
+                               vulnerability_description=shellcode.description, vulnerability_file=shellcode.file,
+                               vulnerability_author=shellcode.author, vulnerability_date=shellcode.date,
+                               vulnerability_type=shellcode.type, vulnerability_platform=shellcode.platform,
+                               file_path=file_path, download_alert=download_alert, shellcode_id=shellcode_id)
     except FileNotFoundError:
         error_msg = 'Sorry! This file does not exist :('
         return render_template('error_page.html', error=error_msg)
--- HoundSploit/searcher/engine/utils.py
+++ HoundSploit/searcher/engine/utils.py
@@ -1,3 +1,6 @@
+import re
+
+
 def check_file_existence(filename):
     try:
         f = open(filename)
@@ -5,4 +8,13 @@ def check_file_existence(filename):
         return True
     except IOError:
         return False
-        
+
+def get_vulnerability_extension(vulnerability_file):
+    """
+    Get the extension of the vulnerability passed as parameter.
+    :param vulnerability_file: the vulnerability we want to get its extension.
+    :return: the extension of the vulnerability passed as parameter.
+    """
+    regex = re.search(r'\.(?P<extension>\w+)', vulnerability_file)
+    extension = '.' + regex.group('extension')
+    return extension        
