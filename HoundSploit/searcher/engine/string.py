0f54a487239254b2e0766b0e8fd9af3749c7b247
Nicolas Carolo <nicolas.carolo@outlook.com>
2020-05-09 22:46:41 +0200

    2.2.1: critical bug in search fixed, bug fix in advanced search, default suggestions updated

--- HoundSploit/searcher/engine/string.py
+++ HoundSploit/searcher/engine/string.py
@@ -20,9 +20,9 @@ def str_is_num_version(str):
 
 def word_is_num_version(str):
     """
-    Check if a string contains a number of version.
-    :param str: the string to check.
-    :return: true if the string contains a number of version, false else.
+    Check if a word contains a number of version.
+    :param str: the word to check.
+    :return: true if the word contains a number of version, false else.
     """
     return bool(re.search(r'\d+((\.\d+)+)?', str))
 
