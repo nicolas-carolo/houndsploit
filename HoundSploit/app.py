14274b93f6369fa029a865791e5881c867ec4780
Nicolas Carolo <nicolas.carolo@outlook.com>
2020-05-15 10:05:18 +0200

    first steps for Windows compatibility

--- HoundSploit/app.py
+++ HoundSploit/app.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import datetime
+import platform
 
 from flask import Flask, render_template, request
 from HoundSploit.searcher.engine.search_engine import search_vulnerabilities_in_db, get_exploit_by_id, get_shellcode_by_id,\
@@ -15,9 +16,14 @@ from HoundSploit.searcher.engine.csv2sqlite import create_db
 from shutil import copyfile
 
 
-init_path = os.path.expanduser("~") + "/HoundSploit"
-template_dir = os.path.abspath(init_path + '/houndsploit/HoundSploit/templates')
-static_folder = os.path.abspath(init_path + '/houndsploit/HoundSploit/static')
+if platform.system == "Windows":
+    init_path = os.path.expanduser("~") + "\HoundSploit"
+    template_dir = os.path.abspath(init_path + '\houndsploit\HoundSploit\\templates')
+    static_folder = os.path.abspath(init_path + '\houndsploit\HoundSploit\static')
+else:
+    init_path = os.path.expanduser("~") + "\HoundSploit"
+    template_dir = os.path.abspath(init_path + '/houndsploit/HoundSploit/templates')
+    static_folder = os.path.abspath(init_path + '/houndsploit/HoundSploit/static')
 app = Flask(__name__, template_folder=template_dir, static_folder=static_folder)
 
 N_RESULTS_FOR_PAGE = 10
--- HoundSploit/searcher/engine/csv2sqlite.py
+++ HoundSploit/searcher/engine/csv2sqlite.py
@@ -1,39 +1,50 @@
 import csv
 import sqlite3
 import os
+import platform
 from HoundSploit.searcher.engine.utils import check_file_existence
 
 
 def create_db():
-    init_path = os.path.expanduser("~") + "/HoundSploit"
-    db_path = init_path + "/hound_db.sqlite3"
+    if platform.system == "Windows":
+        init_path = os.path.expanduser("~") + "\HoundSploit"
+        db_path = init_path + "\hound_db.sqlite3"
+    else:
+        init_path = os.path.expanduser("~") + "/HoundSploit"
+        db_path = init_path + "/hound_db.sqlite3"
     con = sqlite3.connect(db_path)
     cur = con.cursor()
 
     cur.execute("CREATE TABLE searcher_exploit (id, file, description, date, author, type, platform, port);")
     exploits_path = init_path + "/exploitdb/files_exploits.csv"
-    with open(exploits_path, 'r') as fin:
+    with open(exploits_path, 'r', encoding="utf8") as fin:
         dr = csv.DictReader(fin)
         to_db = [(i['id'], i['file'], i['description'], i['date'], i['author'], i['type'], i['platform'], i['port']) for i in dr]
     cur.executemany("INSERT INTO searcher_exploit (id, file, description, date, author, type, platform, port) VALUES (?, ?, ?, ?, ?, ?, ?, ?);", to_db)
 
     cur.execute("CREATE TABLE searcher_shellcode (id, file, description, date, author, type, platform);")
     shellcodes_path = init_path + "/exploitdb/files_shellcodes.csv"
-    with open(shellcodes_path, 'r') as fin:
+    with open(shellcodes_path, 'r', encoding="utf8") as fin:
         dr = csv.DictReader(fin)
         to_db = [(i['id'], i['file'], i['description'], i['date'], i['author'], i['type'], i['platform']) for i in dr]
     cur.executemany("INSERT INTO searcher_shellcode (id, file, description, date, author, type, platform) VALUES (?, ?, ?, ?, ?, ?, ?);", to_db)
 
     cur.execute("CREATE TABLE searcher_suggestion (searched, suggestion, autoreplacement);")
-    suggestions_path = init_path + "/houndsploit/csv/files_suggestions.csv"
-    with open(suggestions_path, 'r') as fin:
+    if platform.system == "Windows":
+        suggestions_path = init_path + "\houndsploit\csv\\files_suggestions.csv"
+    else:
+        suggestions_path = init_path + "/houndsploit/csv/files_suggestions.csv"
+    with open(suggestions_path, 'r', encoding="utf8") as fin:
         dr = csv.DictReader(fin)
         to_db = [(i['searched'], i['suggestion'], i['autoreplacement']) for i in dr]
     cur.executemany("INSERT INTO searcher_suggestion (searched, suggestion, autoreplacement) VALUES (?, ?, ?);", to_db)
 
-    custom_suggestions_path = init_path + "/custom_suggestions.csv"
+    if platform.system == "Windows":
+        custom_suggestions_path = init_path + "\custom_suggestions.csv"
+    else:
+        custom_suggestions_path = init_path + "/custom_suggestions.csv"
     if check_file_existence(custom_suggestions_path):
-        with open(custom_suggestions_path, 'r') as fin:
+        with open(custom_suggestions_path, 'r', encoding="utf8") as fin:
             dr = csv.DictReader(fin)
             to_db = [(i['searched'], i['suggestion'], i['autoreplacement']) for i in dr]
         cur.executemany("INSERT INTO searcher_suggestion (searched, suggestion, autoreplacement) VALUES (?, ?, ?);", to_db)
@@ -41,9 +52,11 @@ def create_db():
     con.commit()
     con.close()
 
+    """
     try:
         f = open(os.path.expanduser("~") + "/HoundSploit/houndsploit_db.lock")
         f.close()
         os.remove(os.path.expanduser("~") + "/HoundSploit/houndsploit_db.lock") 
     except IOError:
         pass
+    """
