160cea318e311a7ce8f94dd36472356e2b79940f
Nicolas Carolo <nicolas.carolo@outlook.com>
2020-05-16 00:01:33 +0200

    2.3.0: migrate for custom_suggestions.csv

--- install_db_linux.sh
+++ install_db_linux.sh
@@ -53,7 +53,7 @@ else
 fi
 
 if [ -d $HOUNDSPLOIT_OLD_PATH  ] ; then
-    if [ -f "$HOUNDSPLOIT_PATH/custom_suggestions.csv" ] ; then
+    if [ -f "$HOUNDSPLOIT_OLD_PATH/custom_suggestions.csv" ] ; then
         cp $HOUNDSPLOIT_OLD_PATH/custom_suggestions.csv $HOUNDSPLOIT_PATH/custom_suggestions.csv
     fi
     if [ -d $HOUNDSPLOIT_OLD_PATH/houndsploit  ] && ! [ -d $HOUNDSPLOIT_OLD_PATH/hsploit  ] ; then
